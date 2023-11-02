7d87a24b1b22cd0074209f3e1a3a03c4223a2773
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-11-02 18:35:08 +0200

    Memory example.

---
+++ 09 Memory/main.js
@@ -0,0 +1,50 @@
+// -=-=-=-=-=-=-=- Memory Management -=-=-=-=-=-=-=-
+let title = document.querySelector('h1');
+title.remove();
+
+// remove title? - No
+
+let number = 10; // memory allocation
+let c = number;  // copy data
+
+++number;       // 11
+console.log(c); // 10
+
+console.log(number);
+
+let user = { id: 1001, login: 'super' }; // memory allocation
+let admin = user; // assign reference
+
+user.id++;          // 1002
+console.log(admin); // ?
+
+// memory usage...
+number = Math.pow(number, 2);
+
+// clear resources
+// GC - Garbage Collector
+
+if (number > 10) {
+    let msg = "Greather than 10";
+    // remove "Greather than 10"? - No
+    console.log(msg);
+}
+
+//console.log(msg); // cannot access msg
+// remove "Greather than 10"? - Yes
+
+number = null;
+// remove 10? - Yes
+console.log(number);
+
+user = null;
+// remove { id: 1001, login: 'super' }? - No
+
+console.log(admin);
+// remove { id: 1001, login: 'super' }? - No
+
+admin = null;
+// remove { id: 1001, login: 'super' }? - Yes
+
+console.log(user); // null
+console.log(admin);// null
